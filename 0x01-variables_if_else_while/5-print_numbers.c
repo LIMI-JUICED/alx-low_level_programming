b3a598485abcdf1ade96ce2bdc1b2a41c4b55c88
limi-juiced <beastdathing@proton.me>
2022-06-06 20:46:16 -0700

    My lovely

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -12,12 +12,12 @@ int main(void)
 {
 	int digit = 0;
 
-	while (digit <=9)
+	while (digit <= 9)
 	{
 		printf("%i", digit);
 		++digit;
 	}
-	print("\n");
+	printf('\n');
 
 	return (0);
 
