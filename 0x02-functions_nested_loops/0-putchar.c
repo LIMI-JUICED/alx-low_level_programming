930f2ba896347bb970195b17d76ab79ff2deb0d9
limi-juiced <beastdathing@proton.me>
2022-06-13 04:36:54 -0700

    My second

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
  * main - Entry Point
  * Description: prints 'Holberton \n'
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char str [] = "Holberton\n";
 int i = 0;
 
 while (str[i] != '\0')
-{
+	{
 	char c str[i];
 
 	_putchar(c);
 	i++;
-}
+	}	
 return (0);
 }
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the valuue of an integer to 98
+ * @n: parameter targeted
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
