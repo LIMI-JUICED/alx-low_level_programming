8d3167f4c80d941b434c0e51a0bdeb95039330c8
limi-juiced <beastdathing@proton.me>
2022-06-13 16:15:40 -0700

    Engineer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,17 +4,17 @@
  * _srtlen - calculate the length of a string.
  *
  * @s: the string calculate it's length.
- * Return: length of a string.
+ * Return: Always (0)
  */
 
 int_srtlen(char *s)
 {
 	int i;
-	int length = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		length++;
+		continue;
 	}
-	return (length);
+
+	return (i);
 }
