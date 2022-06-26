3effb5f7fb7d81b1861e7bdfc74d8036b5dc8951
limi-juiced <beastdathing@proton.me>
2022-06-25 18:08:28 -0700

    recursiooon

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,11 @@
  *
  * Return: recursion
  */
-
 int _strlen_recursion(char *s)
 {
 	if (!*s)
 	{
 		return (0);
 	}
-	return (1 + _strlen_recursion(""));
+	return (1 + _strlen_recursion(++s));
 }
