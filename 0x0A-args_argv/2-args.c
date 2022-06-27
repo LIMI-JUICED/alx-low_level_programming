83d1d332cc51698ed943c33d59883bc94c871be0
limi-juiced <beastdathing@proton.me>
2022-06-27 16:21:12 -0700

    bear

--- 0x0A-args_argv/2-args.c
+++ 0x0A-args_argv/2-args.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints its name
- * 
+ * main - a program that prints all arguments it recieves
+ *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
  *
  * Return: Always 0 (Success)
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	printf("%s\n", argv[i]);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
